fa9cc46371d07fa7bd8892acac2bf062749ff91f
BB-Simon <simon.chowdery@gmail.com>
2023-03-18 22:25:33 +0200

    Add Providers component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,28 @@
+import Providers from '@/components/Providers';
+import { cn } from '@/lib/utils/utils';
 import '@/styles/global.css';
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({
+export default function RootLayout ({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn('bg-white text-slate-900 antailiased', inter.className)}
+    >
+      <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
+        <Providers>
+          {children}
+        </Providers>
+
+        {/* Allow for more hieght on mobile divices */}
+        <div className='h-40 md:hidden' />
+      </body>
     </html>
   )
 }
