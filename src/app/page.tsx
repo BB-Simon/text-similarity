63d89793426f7e8a94f783544148031a94245f38
BB-Simon <simon.chowdery@gmail.com>
2023-03-18 21:56:32 +0200

    Move components, lib and styles dirs to src dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Paragraph from './components/ui/Paragraph'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className='bg-red-500'>
-      <Paragraph size='sm'/>
     </main>
   )
 }
