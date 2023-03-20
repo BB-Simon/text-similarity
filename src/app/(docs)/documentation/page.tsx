6f8193e496f05cfbd0c1b1b4f262626e75351e30
BB-Simon <simon.chowdery@gmail.com>
2023-03-20 02:20:52 +0200

    Add DocumentationTabs component

--- src/app/(docs)/documentation/page.tsx
+++ src/app/(docs)/documentation/page.tsx
@@ -1,11 +1,27 @@
+import Heading from '@/ui/Heading'
+import Paragraph from '@/components/ui/Paragraph'
 import { FC } from 'react'
+import 'simplebar-react/dist/simplebar.min.css'
 
-interface PageProps {
+import DocumentationTabs from '@/components/DocumentationTabs'
+import type { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Similarity API | Documentation',
+  description: 'Free & open-source text similarity API',
 }
 
-const Page: FC<PageProps> = ({ }) => {
-  return <div>Page</div>
+const page: FC = () => {
+  return (
+    <div className='container max-w-7xl mx-auto mt-12'>
+      <div className='flex flex-col items-center gap-6'>
+        <Heading>Making a request</Heading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
+      </div>
+    </div>
+  )
 }
 
-export default Page
+export default page
